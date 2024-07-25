3b71f75116f1225b7455caea18bf4ad46cb9db10
rafael rotiroti <rotirotirafa@gmail.com>
2024-07-24 22:31:39 -0300

    tipos primitivos

---
+++ dart-study/lib/tipos_primitivos.dart
@@ -0,0 +1,20 @@
+String textoTipado = "String completa";
+var texto = "String sem tipagem";
+
+int numerTipado = 1;
+var numero = 10;
+
+var listaNumeros = [1, 2, 3, 4, 5];
+
+var listaStrings = ['Carro', 'Estádio'];
+
+// Adicionando elemento na lista de numeros
+append() {
+  return listaNumeros.add(10);
+}
+
+// Adicionando elemnto na lista de Strings
+
+appendText() {
+  return listaStrings.add("País");
+}
